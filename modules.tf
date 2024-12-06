11976928fcd0683ce846a54d18f1f061b959d088
chinchalinchin <chinchalinchin@gmail.com>
2024-12-06 13:05:00 -0500

    udpate module queries

--- modules.tf
+++ modules.tf
@@ -4,10 +4,10 @@ module "platform" {
   platform              = var.platform
   
   hydration             = {
-    vpc_query           = false
-    subnets_query       = false
-    public_sg_query     = false
-    private_sg_query    = false
-    eks_ami_query       = false
+    acm_cert_query      = true
+  }
+
+  configuration         = {
+    domain_name         = "TODO"
   }
 }
